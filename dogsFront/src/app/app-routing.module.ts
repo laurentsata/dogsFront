03ba2c8c6339c995b29f90b4009e48e16edca85c
john Doe <sataward666@gmail.com>
2022-11-23 10:37:38 +0100

    modif app.-routing & app.component.html

--- dogsFront/src/app/app-routing.module.ts
+++ dogsFront/src/app/app-routing.module.ts
@@ -1,11 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomePageComponent } from './home-page/home-page.component';
+import {AdoptedDogListComponent} from "./home-page/adopted-dog-list/adopted-dog-list.component";
+import {UnadoptedDogListComponent} from "./home-page/unadopted-dog-list/unadopted-dog-list.component";
 
 const routes: Routes = [{
   path: '/' ,
   component: HomePageComponent
-}];
+},
+  {
+    path: '/unadopted' ,
+    component: UnadoptedDogListComponent
+  },
+
+  {
+    path: '/adopted' ,
+    component: AdoptedDogListComponent
+  }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
