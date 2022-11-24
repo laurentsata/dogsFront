e2f07b4cd22337aafca14e1ec296342ae7b22ca8
john Doe <sataward666@gmail.com>
2022-11-24 15:08:54 +0100

    form update

--- dogsFront/src/app/form-update/form-update.component.ts
+++ dogsFront/src/app/form-update/form-update.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { DogService } from '../dog.service';
+import { Router } from '@angular/router';
+import { Dog } from '../models/dog';
 
 @Component({
   selector: 'app-form-update',
   templateUrl: './form-update.component.html',
   styleUrls: ['./form-update.component.sass']
 })
-export class FormUpdateComponent {
+export class FormUpdateComponent implements OnInit{
+  model: Dog = {id: 0, name:"", color:"", imgUrl:"", race:"", isAdopted:false, sex:""};
+  submitted:boolean = false;
+
+  constructor(private service:DogService, private router:Router) { }
+
+
+
+  ngOnInit(): void {
+  }
+
+
+  onSubmit(): void {
+    // form submitted
+    //this.submitted = true;
+    //console.log(this.model);
+    this.service.create(this.model)
+      .subscribe(() => {
+        this.router.navigate(['']);
+      });
+  }
 
 }
