817aaeab43b5fb5dd5b25016eeb6916b8f5bb8bf
Elina44 <103180214+Elina44@users.noreply.github.com>
2022-11-24 16:48:12 +0100

    Mise en place des boutons pour les chiens déjà adoptés

--- dogsFront/src/app/home-page/adopted-dog-list/adopted-dog-item/adopted-dog-item.component.ts
+++ dogsFront/src/app/home-page/adopted-dog-list/adopted-dog-item/adopted-dog-item.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { Router } from '@angular/router';
+import { DogService } from 'src/app/dog.service';
 import { Dog } from 'src/app/models/dog';
 
 @Component({
@@ -11,10 +13,15 @@ export class AdoptedDogItemComponent implements OnInit {
   @Input()
   dog!: Dog;
 
-  constructor() { }
+  constructor(private dogService: DogService, private router: Router) { }
 
   ngOnInit(): void {
   }
 
-}
+  delete(id: number): void {
+    this.dogService.deleteDog(id).subscribe((rep) =>
+    //this.router.navigateByUrl('/'));
+    window.location.reload())
+  }
 
+}
